33a099c7de98318a9a93e1afd50f55570bfc640b
Watson,Kelvin <kelvincswatson@gmail.com>
2017-09-13 00:13:58 -0400

    remove logging, fix image width

--- src/Components/LiveStreams.js
+++ src/Components/LiveStreams.js
@@ -30,19 +30,17 @@ const LiveStreams = ({ isFetching, status, liveStreams, game }) => {
             <div key={stream.channel.url} className="LiveStreams-link-override column col-3 col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
               <div className="card LiveStreamCard">
                 <a href={stream.channel.url} target="_blank" className="card-image">
-                  <img className="img-responsive" src={stream.preview.medium}
+                  <img className="img-responsive livestreams-img-responsive-override" src={stream.preview.medium}
                     alt="Stream channel preview"/>
                 </a>
                 <div className="card-header">
                   <a href={stream.channel.url} target="_blank" className="btn btn-secondary float-right">
                     <i className="icon icon-share"></i>
                   </a>
-                  <div className="card-title h5">{stream.channel.display_name} </div>
+                  <div className="card-title h6">{stream.channel.display_name} </div>
                   <div className="card-subtitle text-gray">Streaming: {stream.game}</div>
                 </div>
-                <div className="card-body">
-                  Stream title: {stream.channel.status}<br/>
-                </div>
+                <div className="card-body">"{stream.channel.status}"</div>
               </div>
             </div>
           ))}

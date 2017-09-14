8e862c1e214a53960db5ade6082bb9a91908d95f
Watson,Kelvin <kelvincswatson@gmail.com>
2017-09-14 16:27:11 -0400

    comments or suggestions

--- src/Components/AppFooter.js
+++ src/Components/AppFooter.js
@@ -7,7 +7,10 @@ const AppHeader = () => (
      <a href="/" className="navbar-brand mr-2">&copy; GAYMER STREAMS 2017</a>
      {/*<a href="#" className="btn btn-link navbar-slogan">Find Gaymers Bears streaming your favorite games!</a>*/}
     </section>
-    <section className="navbar-section navbar-right-section">
+    <section className="navbar-section navbar-right-section app-footer-navbar-right-section-override">
+
+      <span>Comments/Suggestions? &nbsp;</span>
+
       <a href="https://twitter.com/Vinnie_Panda"
         className="twitter-follow-button"
         data-show-count="false" data-size="large">Follow @Vinnie_Panda</a>
